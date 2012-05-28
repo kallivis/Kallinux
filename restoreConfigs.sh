139d462aa98f7cdeb910b97d8e29e5be3c25ac95
Cody McCarthy <cody@colibri-software.com>
2012-05-28 11:47:20 -0300

    shell script done, konsole added, oh-my-zsh added, pre bash/zsh seperation

--- restoreConfigs.sh
+++ restoreConfigs.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-mkdir ~/tmp
-mkdir ~/tmp/.vim/
+mkdir -p ~/tmp
+mkdir -p ~/tmp/.vim/
+mkdir -p .git
+mkdir -p ~/.kde/share/apps
 ln -s -f ~/mySync/.bash_aliases ~/.bash_aliases
 ln -s -f ~/mySync/.bash_logout ~/.bash_logout
 ln -s -f ~/mySync/.bash_profile ~/.bash_profile
+ln -s -f ~/mySync/.bash_profile ~/.profile
 ln -s -f ~/mySync/.bashrc ~/.bashrc
 ln -s -f ~/mySync/.emacs ~/.emacs
-ln -s -f ~/mySync/.gitconfig ~/gitconfig
-ln -s -f ~/mySync/.gitk ~/.gik
+ln -s -f ~/mySync/.gitconfig ~/.gitconfig
+ln -s -f ~/mySync/.gitk ~/.gitk
 ln -s -f ~/mySync/.gvimrc ~/.gvimrc
 ln -s -f ~/mySync/.vimrc ~/.vimrc
 ln -s -f ~/mySync/.zshrc ~/.zshrc
 ln -s -f ~/mySync/.zshenv ~/.zshenv
-ln -s -f ~/mySync/.zlogin ~/.zlogin
+ln -s -f ~/mySync/.tmux.conf ~/.tmux.conf
+ln -s -f ~/mySync/konsole/ ~/.kde/share/apps
+ln -s -f ~/mySync/.oh-my-zsh/ ~/
 source ~/.bashrc
 source ~/.zshrc
 
