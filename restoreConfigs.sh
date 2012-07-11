09b9c32bd880a31a6172b34039c408445248aa83
Cody McCarthy <cody@colibri-software.com>
2012-07-11 14:53:04 -0300

    aliases added for bundle and gitclone oh my zsh for restore script

--- restoreConfigs.sh
+++ restoreConfigs.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/tmp
 mkdir -p ~/tmp/.vim/
 mkdir -p ~/.git
-#mkdir -p ~/.kde/share/apps
+mkdir -p ~/.kde/share/apps
 #rm -rf ~/.kde/share/apps/konsole/
 ln -s -f ~/mySync/.bash_aliases ~/.bash_aliases
 ln -s -f ~/mySync/.bash_logout ~/.bash_logout
@@ -20,4 +20,5 @@ ln -s -f ~/mySync/.tmux.conf ~/.tmux.conf
 ln -s -f ~/mySync/konsole/ ~/.kde/share/apps
 ln -s -f ~/mySync/.oh-my-zsh/ ~/
 ln -s -f ~/mySync/shellConfs/ ~/
+git clone https://github.com/kallivis/oh-my-zsh.git ~/.oh-my-zsh
 source ~/.zshrc
