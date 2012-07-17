2c212fddcd98abb3334801b7a661f4f54095c0d3
Cody McCarthy <cody@colibri-software.com>
2012-07-16 23:25:02 -0300

    changed main dir from mySync to Kallinux

--- restoreConfigs.sh
+++ restoreConfigs.sh
@@ -4,21 +4,21 @@ mkdir -p ~/tmp/.vim/
 mkdir -p ~/.git
 mkdir -p ~/.kde/share/apps
 #rm -rf ~/.kde/share/apps/konsole/
-ln -s -f ~/mySync/.bash_aliases ~/.bash_aliases
-ln -s -f ~/mySync/.bash_logout ~/.bash_logout
-ln -s -f ~/mySync/.bash_profile ~/.bash_profile
-ln -s -f ~/mySync/.bash_profile ~/.profile
-ln -s -f ~/mySync/.bashrc ~/.bashrc
-ln -s -f ~/mySync/.emacs ~/.emacs
-ln -s -f ~/mySync/.gitconfig ~/.gitconfig
-ln -s -f ~/mySync/.gitk ~/.gitk
-ln -s -f ~/mySync/.gvimrc ~/.gvimrc
-ln -s -f ~/mySync/.vimrc ~/.vimrc
-ln -s -f ~/mySync/.zshrc ~/.zshrc
-ln -s -f ~/mySync/.zshenv ~/.zshenv
-ln -s -f ~/mySync/.tmux.conf ~/.tmux.conf
-ln -s -f ~/mySync/konsole/ ~/.kde/share/apps
-ln -s -f ~/mySync/.oh-my-zsh/ ~/
-ln -s -f ~/mySync/shellConfs/ ~/
-git clone https://github.com/kallivis/oh-my-zsh.git ~/.oh-my-zsh
+ln -s -f ~/Kallinux/.bash_aliases ~/.bash_aliases
+ln -s -f ~/Kallinux/.bash_logout ~/.bash_logout
+ln -s -f ~/Kallinux/.bash_profile ~/.bash_profile
+ln -s -f ~/Kallinux/.bash_profile ~/.profile
+ln -s -f ~/Kallinux/.bashrc ~/.bashrc
+ln -s -f ~/Kallinux/.emacs ~/.emacs
+ln -s -f ~/Kallinux/.gitconfig ~/.gitconfig
+ln -s -f ~/Kallinux/.gitk ~/.gitk
+ln -s -f ~/Kallinux/.gvimrc ~/.gvimrc
+ln -s -f ~/Kallinux/.vimrc ~/.vimrc
+ln -s -f ~/Kallinux/.zshrc ~/.zshrc
+ln -s -f ~/Kallinux/.zshenv ~/.zshenv
+ln -s -f ~/Kallinux/.tmux.conf ~/.tmux.conf
+ln -s -f ~/Kallinux/konsole/ ~/.kde/share/apps
+ln -s -f ~/Kallinux/.oh-my-zsh/ ~/
+ln -s -f ~/Kallinux/shellConfs/ ~/
+git clone https://github.com/kallivis/oh-my-zsh.git ~/Kallinux/.oh-my-zsh
 source ~/.zshrc
