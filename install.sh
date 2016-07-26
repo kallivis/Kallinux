9e528c9334d219984c498fb00dda6d3d62fbc8b7
Cody McCarthy <cody@colibri-software.com>
2016-07-26 11:49:08 -0300

    Add node install

--- install.sh
+++ install.sh
@@ -6,4 +6,9 @@ sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install -y tmux=2.0-1~ppa1~t
 sudo npm install bower -g
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+nvm install 5.0
+nvm use 5.0
+nvm alias default node
+
 
